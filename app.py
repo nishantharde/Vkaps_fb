426dc2850043884e8ac3a3e061c0bf25a38cafc2
nishant harde <nishantharde022@gmail.com>
2024-05-25 23:31:21 +0530

    modifyed

---
+++ app.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+import streamlit as st
+client =OpenAI(api_key=OPENAI_API_KEY) 
+# Upload a file with an "assistants" purpose
+file = client.files.create(
+  file=open("speech.py", "rb"),
+  purpose='assistants'
+)
+
+# Create an assistant using the file ID
+assistant = client.beta.assistants.create(
+  instructions="You are a personal math tutor. When asked a math question, write and run code to answer the question.",
+  model="gpt-4-turbo-preview",
+  tools=[{"type": "code_interpreter"}],
+  file_ids=[file.id]
+) 
--- app2.py
+++ app2.py
@@ -3,13 +3,7 @@ from dotenv import load_dotenv
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_openai import ChatOpenAI
-import facebook
-import os
 load_dotenv()
-
-Open_AI_Key = os.environ["OPENAI_API_KEY"] 
-FB_Access_Token = os.environ["FB_GRAPH_ACCESS_TOKEN"]
-FB_Page_ID = os.environ["FB_Page_ID"]
 
 st.set_page_config(page_title="URL Summirazer", page_icon=":face:")
 st.markdown("<h1 style='text-align: center;'>URL TO MEDIA_POST</h1>", unsafe_allow_html=True)
@@ -29,17 +23,3 @@ if st.session_state['URL'] !="":
   docs = loader.load()
   summarize_text=chain.invoke(docs)
   std = summarize_text.get("output_text")
-
-send_button = st.button("Post on Facebook page",key="Post")
-
-if summarise_button:
-  summarise_placeholder = st.write(std)
-if send_button:
-  access_token = 'FB_GRAPH_ACCESS_TOKEN'
-  graph = facebook.GraphAPI(access_token)
-  page_id = "FB_Page_ID" 
-  post_message = std
-  graph.put_object(page_id, "feed", message=post_message)
-
-  # print("Post successfully sent to Facebook.")
-  st.write("Post successfully sent to Facebook.")
